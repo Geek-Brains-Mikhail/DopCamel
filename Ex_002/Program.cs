336dcb4c44d59f7718a5eff7ad78dc4522dce948
Mikhail Malafeew <malafeew2016@gmail.com>
2022-08-06 18:13:08 +0300

    Ex_002

---
+++ Ex_002/Program.cs
@@ -0,0 +1,12 @@
+﻿//2. Есть программа с бесконечным циклом. Когда пользователь вводит exit программа заканчивается
+
+bool boolean = true;
+string decision;
+while(boolean){
+    Console.Write("Может быть закончим этот беспредел?(Если согласен, то напиши - 'Exit'): ");
+    decision = Console.ReadLine();
+    if( decision.ToLower() == "exit"){
+        boolean = false;
+    }
+}
+Console.WriteLine("Ура!");
